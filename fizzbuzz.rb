be634d306f492112e71283647ef4536dc334b639
Joshua Palacios <palacios.joshua@gmail.com>
2017-10-25 02:14:57 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+def fizzbuzz(num)
+if num % 3 == 0
+  puts "fizz"
+end 
+elsif num % 5 == 0
+  puts "buzz"
+end  
+elsif (num % 3 == 0 && num % 5 == 0)
+  puts "fizzbuzz"
+end
+else 
+  puts nil
+end
